ed49fa85a0a65fa980606bb4e0cd24ef9fb4717d
111blackzero <111blackzero@gmail.com>
2022-06-23 16:37:07 +0900

    N fix number

--- KarutaGunma/DSP.cpp
+++ KarutaGunma/DSP.cpp
@@ -173,8 +173,8 @@ int fftWav(WavFile* pwavFile)
     pwavFile->load();
 
     //Do DSP here
-    const int N = pwavFile->pSNDfile->frames();
-
+   // const int N = static_cast<int>(pwavFile->pSNDfile->frames()) ;
+    const int N = 32767*2*2;
 
     fftw_complex* in = static_cast<fftw_complex*>(fftw_malloc(sizeof(fftw_complex) * N));
     fftw_complex* out = static_cast<fftw_complex*>(fftw_malloc(sizeof(fftw_complex) * N));
